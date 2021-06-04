6118d036482600711da4ef35acb7b94cba64297a
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-06-04 10:58:26 +0200

    Fixing the width and height arrangement

--- Problem-2.pl
+++ Problem-2.pl
@@ -26,8 +26,35 @@ printGrid(GW,GH,C,[H|T]):-
 		;  printGrid(GW,GH,A,T)
 	).
 
+
+% reversing list
+rev(L, R):- rev(L, R, []).
+
+rev([], L, L).
+rev([H|T], L, Tmp):-
+rev(T, L, [H|Tmp]).
+
+% spliting interface
+splt(L, N, R1, R2):-
+splt_(L, N, L1, R2),
+rev(L1, R1).
+
+% splitting function
+splt_(L, 0, [], L).
+
+splt_([H|T], N, L1, L2):-
+NewN is N - 1,
+splt_(T, NewN, LR, L2),
+append(LR, [H], L1).
+
+% add a dot
+dot(L,[],G,G2):-!.
+dot(L,[H|T],G,G2):-
+	splt([1, 2, 3, 4, 5], H, L1, L2).
+
 % test go(4,3,[3,1],[2,2]).
-go(GW,GH,A,B):-
+go(GH,GW,A,B):-
 	grid(GW,GH,G),
 	write("Game:"), nl,
 	printGrid(GW,GH,0,G).
+	%dot('G', A, G, NewG).
